cc4e948809aa4c830d583365b8c409a96e7a4c5f
Henry Soto <hsotolima@frba.utn.edu.ar>
2023-04-12 21:59:22 -0300

    Retoques para cumplir formato de TP

---
+++ 00-Hello/tp0.c
@@ -0,0 +1,13 @@
+/*Hello World
+First output on C language
+Author: Henry Soto
+Last mod: 12/04/2023
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Hello world!\n");
+    return 0;
+}
--- tp0/tp0.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num;
-    printf("Hola mundo!\n");
-    return 0;
-}
